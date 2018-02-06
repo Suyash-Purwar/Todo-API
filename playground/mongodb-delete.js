9696bfe8dcac0bf18c69138ae175d87fc4d37f20
Suyash-Purwar <purwarsuyash06@gmail.com>
2018-02-06 15:32:14 +0530

    Added mongodb-update script

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -6,26 +6,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, db) => {
     } else {
         console.log('Connected to MongoDB server.')
 
-        // db.collection('ToDo').deleteMany({text: 'Something Lorem Ipsum'}).then(result => {
-        //     console.log(JSON.stringify(docs, undefined, 2));
-        // }, error => {
-        //     console.log('Unable to read docs');
-        // });
+        db.collection('ToDo').deleteMany({text: 'Something Lorem Ipsum'}).then(result => {
+            console.log(JSON.stringify(docs, undefined, 2));
+        }, error => {
+            console.log('Unable to read docs');
+        });
 
-        // db.collection('ToDo').deleteOne({text: 'I have to study'}).then(result => {
-        //     console.log(result);
-        // });
+        db.collection('ToDo').deleteOne({text: 'I have to study'}).then(result => {
+            console.log(result);
+        });
 
-        // db.collection('Users').findOneAndDelete({name: 'Mansi Purwar'}).then(result => {
-        //     console.log(result.value);
-        //     console.log(result);
-        // })
+        db.collection('Users').findOneAndDelete({name: 'Mansi Purwar'}).then(result => {
+            console.log(result.value);
+            console.log(result);
+        })
 
-        // db.collection('Users').deleteMany({name: 'Suyash Purwar'}).then(result => {
-        //     console.log(result);
-        // }, error => {
-        //     console.log('Unable to delete query from database');
-        // });
+        db.collection('Users').deleteMany({name: 'Suyash Purwar'}).then(result => {
+            console.log(result);
+        }, error => {
+            console.log('Unable to delete query from database');
+        });
 
         db.collection('Users').findOneAndDelete({name: 'Shubham Purwar'}).then(result => {
             console.log(result);
